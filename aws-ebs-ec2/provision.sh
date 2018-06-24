95672a25ef9f91bdb7dbca4975efe009a70228f5
William Tsoi <william@williamtsoi.net>
2018-06-25 01:33:58 +1000

    more steps to install apache etc

--- aws-ebs-ec2/provision.sh
+++ aws-ebs-ec2/provision.sh
@@ -2,4 +2,17 @@
 
 set -e
 
-sudo apt-get -y update
+# Upgrade to latest
+sudo yum update -y
+sudo yum upgrade -y
+
+# Install Apache
+sudo yum install httpd mod_ssl -y
+sudo echo ServerName demo >> /etc/httpd/conf/httpd.conf
+sudo iptables -I INPUT -p tcp --dport 80 -j ACCEPT
+sudo service iptables save
+sudo /sbin/chkconfig httpd on
+
+# Install PHP
+sudo yum install php php-mysql php-devel php-gd php-pecl-memcache php-pspell php-snmp php-xmlrpc php-xml -y
+
