f6dc0c55aa6312a95c590ce9fc32c0789bfa3011
William Tsoi <william@williamtsoi.net>
2018-06-25 01:37:07 +1000

    fix

--- aws-ebs-ec2/provision.sh
+++ aws-ebs-ec2/provision.sh
@@ -8,7 +8,7 @@ sudo yum upgrade -y
 
 # Install Apache
 sudo yum install httpd mod_ssl -y
-sudo echo ServerName demo >> /etc/httpd/conf/httpd.conf
+echo 'ServerName demo' | sudo tee --append /etc/httpd/conf/httpd.conf
 sudo iptables -I INPUT -p tcp --dport 80 -j ACCEPT
 sudo service iptables save
 sudo /sbin/chkconfig httpd on
